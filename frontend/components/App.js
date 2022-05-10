4c1247151dc0fc94ace62d060d2bf9f2ded3d115
Andrew Gomez <drewgomez209@gmail.com>
2022-05-10 15:03:09 -0700

    html set

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,9 +3,31 @@ import React from 'react'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  state = {
+    todos: [],
+    error: "",
+    todoNameInput: "",
+    displayCompleted: true,
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <h1>Todo App</h1>
+        <div id="error">Error: No error here</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          <div>Walk the dog</div>
+          <div>Finish MyAdmin Project</div>
+        </div>
+        <form>
+          <input type="text" placeholder="Type Todo"></input>
+          <input type="submit"></input>
+        </form>
+        <button>Clear Completed</button>
+      </div>
+    )
   }
 }
 
-// initial push
